42e4a7a2cd9eeecda674bde34b3804dbb064103a
shaeeb <shaeebali@gmail.com>
2022-08-24 11:35:28 +0100

    server setup complete, responding to API calls

--- server/app.js
+++ server/app.js
@@ -10,7 +10,7 @@ const results = [
     {id: 1, title: "Broccoli Soup", snippet: "Learn how to make this soup"},
     {id: 2, title: "Broccoli Soup", snippet: "Learn how to make this soup"},
     {id: 3, title: "Broccoli Soup", snippet: "Learn how to make this soup"},
-    {id: 4, title: "Broccoli Soup", snippet: "Learn how to make this soup"},
+    {id: 4, title: "Lentil Soup", snippet: "Learn how to make this soup"},
     {id: 5, title: "Broccoli Soup", snippet: "Learn how to make this soup"},
     {id: 6, title: "Broccoli Soup", snippet: "Learn how to make this soup"},
     {id: 7, title: "Broccoli Soup", snippet: "Learn how to make this soup"},
@@ -20,7 +20,7 @@ const results = [
 ]; 
 
 //send data to server to check connection
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
@@ -29,7 +29,20 @@ app.get('/results', (req, res) => {
     res.json(results)
 })
 
+// route to retrieve results by their IDs and throw error if incorrect
+app.get('/results/:id', (req, res) => {
+    try {
+        const resultId = parseInt(req.params.id)
+        const selectedResult = results.find(c => c.id === resultId)
+        if(!selectedResult) {
+            throw new Error('This result does not exist, choose a number between 1 and 10, thanks.')
+        }
+        res.send(selectedResult)
+    } catch(err) {
+        res.status(404).send({message: err.message})
+    }
+})
 
 
 // export modules:
-module.export = app;
+module.exports = app;
